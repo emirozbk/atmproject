c1f362477d43684fb2ec17c6cdef5e5b0c28874c
Emir <emirozbek789@gmail.com>
2024-07-25 10:23:35 +0300

    backend için ilk commit

---
+++ src/test/java/com/bbs/atmproje/AtmprojeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbs.atmproje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmprojeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
